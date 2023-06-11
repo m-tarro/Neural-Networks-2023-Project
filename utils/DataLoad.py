142aa62425eb4b32216c4aa9461605569d699a2d
Martti Tarro <116387655+m-tarro@users.noreply.github.com>
2023-06-11 19:49:33 +0300

    Update DataLoad.py

--- utils/DataLoad.py
+++ utils/DataLoad.py
@@ -104,11 +104,9 @@ class DataLoad(tf.data.TFRecordDataset):
         dataset = dataset.repeat() # the training dataset must repeat for several epochs
         if not ordered:
             dataset = dataset.shuffle(2048)
-        if cutmixup:
-            dataset = dataset.batch(self.BATCH_SIZE)
-            dataset = dataset.map(lambda x, y: data_augment([x, y]), num_parallel_calls=self.AUTO)
-            dataset = dataset.unbatch()
         dataset = dataset.batch(self.BATCH_SIZE)
+        if cutmixup:
+            dataset = dataset.map(lambda x, y: data_augment([x, y]), element_spec=tf.TensorSpec(shape=(None,)), num_parallel_calls=self.AUTO)
         dataset = dataset.prefetch(self.AUTO) # get next batch while training
         return dataset
     
